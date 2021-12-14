c8f939848ad8e5c0f432b5a30239c0e5bf862045
Yohanes <yohanesvelly23@gmail.com>
2021-12-14 09:40:50 +0700

    Slicing Login Page

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {ILLogo} from '../../assets';
+import {Button, Gap, Input, Link} from '../../components';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login Page</Text>
+    <View style={styles.container}>
+      <ILLogo />
+      <Text style={styles.textTitle}>Masuk dan mulai berkonsultasi</Text>
+      <Input label="Email Address" />
+      <Gap height={24} />
+      <Input label="Password" />
+      <Gap height={10} />
+      <Link title="Forgot My Password" size={12} />
+      <Gap height={40} />
+      <Button title="Sign In" />
+      <Gap height={30} />
+      <Link title="Create New Account" size={16} align="center" />
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    padding: 40,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  textTitle: {
+    marginVertical: 40,
+    maxWidth: 150,
+    fontSize: 20,
+    color: '#112340',
+    fontFamily: 'Nunito-SemiBold',
+  },
+});
